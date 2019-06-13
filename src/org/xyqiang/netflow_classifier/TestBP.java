9eac0b423b501cd7c1a162c8b698d2c0a2bb611d
Yuqiang Xie <IndexFziQ@gmail.com>
2019-06-13 14:40:12 +0800

    Update TestBP.java

--- src/org/xyqiang/netflow_classifier/TestBP.java
+++ src/org/xyqiang/netflow_classifier/TestBP.java
@@ -17,22 +17,22 @@ public class TestBP {
 
 		MultilayerPerceptron mp=new MultilayerPerceptron();
 
-		//ÉèÖÃÈë²Î
-		mp.setGUI(false);//ÊÇ·ñ½øÐÐÍ¼ÐÎ½»»¥
-		mp.setAutoBuild(true);//ÉèÖÃÍøÂçÖÐµÄÁ¬½ÓºÍÒþ²ã
-		mp.setDebug(false);//¿ØÖÆ´òÓ¡ÐÅÏ¢
-		mp.setDecay(false);//Èç¹ûÎªtrue»á½µµÍÑ§Ï°ËÙÂÊ
-		mp.setHiddenLayers("a");//¶ÔÔ¤²â½á¹û¼¸ºõÃ»ÓÃÓ°Ïì
-		mp.setLearningRate(0.3);//Weights±»¸üÐÂµÄÊýÁ¿,¶ÔÔ¤²â½á¹ûÓ°ÏìºÜ´ó
-		mp.setMomentum(0.8);//µ±¸üÐÂweightsÊ±ÉèÖÃµÄ¶¯Á¿
-		mp.setNormalizeAttributes(true);//¿ÉÒÔÓÅ»¯ÍøÂçÐÔÄÜ
-		mp.setNormalizeNumericClass(true);//Èç¹ûÔ¤²âµÄÊÇÊýÖµÐÍ¿ÉÒÔÌá¸ßÍøÂçµÄÐÔÄÜ
-		mp.setReset(false);//±ØÐëÒªÔÚAutoBuildÎªtrueµÄÌõ¼þÏÂ½øÐÐÉèÖÃ·ñÔòÄ¬ÈÏ¼´¿É
-		mp.setSeed(0);//Ëæ»úÖÖ×ÓÊý£¬¶ÔÔ¤²â½á¹ûÓ°Ïì´ó
-		mp.setTrainingTime(300);//µü´úµÄ´ÎÊý,ÓÐÒ»¶¨Ó°Ïì£¬µ«ÊÇ²»´ó
-		mp.setValidationSetSize(20);//ÑéÖ¤°Ù·Ö±È£¬Ó°Ïì´ó
-		mp.setValidationThreshold(50);//¼¸ºõÃ»ÓÃÓ°Ïì
-		mp.setNominalToBinaryFilter(true);//¿ÉÒÔÌá¸ßÐÔÄÜ
+
+		mp.setGUI(false);
+		mp.setAutoBuild(true);
+		mp.setDebug(false);
+		mp.setDecay(false);
+		mp.setHiddenLayers("a");
+		mp.setLearningRate(0.3);
+		mp.setMomentum(0.8);
+		mp.setNormalizeAttributes(true);
+		mp.setNormalizeNumericClass(true);
+		mp.setReset(false);
+		mp.setSeed(0);
+		mp.setTrainingTime(300);
+		mp.setValidationSetSize(20);
+		mp.setValidationThreshold(50);
+		mp.setNominalToBinaryFilter(true);
 
 		mp.buildClassifier(train);
 		SerializationHelper.write("./data/Model/MultilayerPerceptron.model", mp);
@@ -40,9 +40,9 @@ public class TestBP {
 		Evaluation eval=new Evaluation(train);
 		eval.evaluateModel(mp, test);
 
-		System.out.println("Æ½¾ù¾ø¶ÔÎó²î£º"+eval.meanAbsoluteError());//Ô½Ð¡Ô½ºÃ
-		System.out.println("¾ù·½¸ùÎó²î£º"+eval.rootMeanSquaredError());//Ô½Ð¡Ô½ºÃ
-		System.out.println(mp.toString());//¿´²»³öÄ£ÐÍµÄÒâË¼
+		System.out.println(eval.meanAbsoluteError());
+		System.out.println(eval.rootMeanSquaredError());
+		System.out.println(mp.toString());
 
 
 	}
